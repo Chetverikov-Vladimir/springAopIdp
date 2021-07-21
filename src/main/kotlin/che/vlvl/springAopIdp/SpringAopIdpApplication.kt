cc7dfb09cc03b4844476aff1ec91445ca1009cd3
Chetverikov-Vladimir <vladimir.che@outlook.com>
2021-07-21 11:24:38 +0300

    Initial commit

---
+++ src/main/kotlin/che/vlvl/springAopIdp/SpringAopIdpApplication.kt
@@ -0,0 +1,11 @@
+package che.vlvl.springAopIdp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAopIdpApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAopIdpApplication>(*args)
+}
---
+++ src/test/kotlin/che/vlvl/springAopIdp/SpringAopIdpApplicationTests.kt
@@ -0,0 +1,13 @@
+package che.vlvl.springAopIdp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAopIdpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
